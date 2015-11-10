9850382304ce0e7a49b23f8688c8cf60c408acb9
Robert Nowak <robert.andreas.nowak@gmail.com>
2015-11-10 21:00:59 +0100

    Initial Commit

---
+++ Website1.js
@@ -0,0 +1,15 @@
+// programmatic representation of a webpage = DOM
+
+// onclick="addNums ()
+
+// alert("")
+
+// function change ()
+
+
+
+// function Name(firstname, lastname){
+// 	return "fistname" "lastname"
+// 	alert(Name("Robert", "Nowak")
+// }
+
